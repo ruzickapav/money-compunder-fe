9d077979143ab25f932d9ffd54b0102efbc41bfb
pavelruzicka <pavel.ruzicka5@gmail.com>
2022-10-30 11:25:14 +0100

    Initial structure

---
+++ src/components/form/CustomField.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import {TextField} from "@material-ui/core";
+import {FieldProps} from "formik";
+
+interface Props extends FieldProps{
+    placeholder: string;
+}
+
+export const CustomField: React.FC<Props> = ({placeholder, field}) => {
+    return (
+    <TextField placeholder={placeholder} {...field}/>
+    )
+}
