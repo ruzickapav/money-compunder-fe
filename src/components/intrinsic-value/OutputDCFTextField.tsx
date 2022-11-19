7b5f51440d4e9adeafde3c3708636abc32a5c066
pavelruzicka <pavel.ruzicka5@gmail.com>
2022-11-19 14:24:51 +0100

    fixup! fixup! fixup! Initial structure

--- src/components/intrinsic-value/OutputDCFTextField.tsx
+++ src/components/intrinsic-value/OutputDCFTextField.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {useRecoilValue} from "recoil";
 import {intrinsicValueInput} from "../../states/IntrinsicInput";
 import {keyMetricsQuery, stockOverviewSelector} from "../../states/StockStats";
-import {TextField} from "@mui/material";
+import {NumericFormat} from 'react-number-format';
 
 export const OutputDFCF: React.FC = () => {
     const keyMetrics = useRecoilValue(keyMetricsQuery);
@@ -14,9 +14,10 @@ export const OutputDFCF: React.FC = () => {
     if (!(stockOverviewValue.marketCap === null || keyMetrics.freeCashFlow === null || stockOverviewValue.price == null || intrinsicValueInputParams === null)) {
         const horizonYears = 10.0
         const futureEarningsPerShare = keyMetrics.freeCashFlowPerShare * Math.pow(1.0 + (intrinsicValueInputParams.growth / 100.0), horizonYears)
-        console.log("Earnings per share after 10 years")
-        value = futureEarningsPerShare * intrinsicValueInputParams.terminalMultiple * Math.pow((1.0 - intrinsicValueInputParams.expectedReturn), horizonYears)
+        console.log(`Earnings per share after 10 years ${futureEarningsPerShare}`)
+        console.log(`Stock price per share after 10 years ${futureEarningsPerShare * intrinsicValueInputParams.terminalMultiple}`)
+        value = futureEarningsPerShare * intrinsicValueInputParams.terminalMultiple * Math.pow(1.0 - (intrinsicValueInputParams.expectedReturn / 100), horizonYears - 1)
     }
 
-    return <TextField id="intrinsic-value" label="intrinsicValue" value={value}/>;
+    return <NumericFormat value={value} allowLeadingZeros thousandSeparator="," decimalScale={2}/>
 }
